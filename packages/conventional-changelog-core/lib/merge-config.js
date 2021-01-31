46f5eb7dc2e36e3e558c39d003dc198a3cd51f78
Armano <armano2@users.noreply.github.com>
2021-01-31 21:34:47 +0100

    refactor: use native typeof instead of lodash/isBoolean

--- packages/conventional-changelog-core/lib/merge-config.js
+++ packages/conventional-changelog-core/lib/merge-config.js
@@ -213,7 +213,7 @@ function mergeConfig (options, context, gitRawCommitsOpts, parserOpts, writerOpt
         }
       }
 
-      if (!_.isBoolean(options.outputUnreleased)) {
+      if (typeof options.outputUnreleased !== 'boolean') {
         options.outputUnreleased = true
       }
 
@@ -323,7 +323,7 @@ function mergeConfig (options, context, gitRawCommitsOpts, parserOpts, writerOpt
             }
           }
 
-          if (!_.isBoolean(context.linkCompare) && context.previousTag && context.currentTag) {
+          if (typeof context.linkCompare !== 'boolean' && context.previousTag && context.currentTag) {
             context.linkCompare = true
           }
 
